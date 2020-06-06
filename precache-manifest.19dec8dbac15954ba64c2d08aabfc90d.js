21519bc4de47f171c1e5d8b6b7a15419ef93dc6f
ankit_brijwasi <abrijwasi1@gmail.com>
2020-06-07 01:50:49 +0530

    Updates

---
+++ precache-manifest.19dec8dbac15954ba64c2d08aabfc90d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb47f0fdd0de4287c4f474a9fdff032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac045477f1956ffe9396",
+    "url": "/static/css/2.67246356.chunk.css"
+  },
+  {
+    "revision": "65961347f62f7a401129",
+    "url": "/static/css/main.564974f9.chunk.css"
+  },
+  {
+    "revision": "ac045477f1956ffe9396",
+    "url": "/static/js/2.a3787032.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/2.a3787032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444c08903b93a4bf408a",
+    "url": "/static/js/3.4e533e3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/3.4e533e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e729f9ffb4db57ac962",
+    "url": "/static/js/4.8317df13.chunk.js"
+  },
+  {
+    "revision": "65961347f62f7a401129",
+    "url": "/static/js/main.d530c076.chunk.js"
+  },
+  {
+    "revision": "1d9f436b8a7978e4c067",
+    "url": "/static/js/runtime-main.256b7852.js"
+  },
+  {
+    "revision": "17521b060568e8e468a2b4866daf8c76",
+    "url": "/static/media/Group-27.17521b06.png"
+  },
+  {
+    "revision": "59a1bc80d5bf1e859a676d40e49c5f5f",
+    "url": "/static/media/about-pic.59a1bc80.png"
+  },
+  {
+    "revision": "771545b6612db7a78a85ae8489ef8ec2",
+    "url": "/static/media/hero_1.771545b6.png"
+  },
+  {
+    "revision": "de163e51389ec3884c6468dae46a27bd",
+    "url": "/static/media/icon.de163e51.svg"
+  }
+]);
